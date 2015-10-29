9ae1a19f35ebff17e28ea01615687093af039cde
mputman98 <mrputman@NN121-HA-12.HS.sd203.org>
2015-10-29 10:30:39 -0500

    Putman 10/29
    nested loops practice

---
+++ Chapter6prac/nestLoops.java
@@ -0,0 +1,14 @@
+
+
+
+public class nestLoops
+{
+    public static void main(String[] args){
+        for (int val = 1; val <= 2; val++){
+            for(int val2 = 1; val2 <= 4; val2++){
+                System.out.println(val + " " + val2);
+            }
+        }
+    }
+}
+    
---
+++ Chapter6prac/subStrings.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class subStrings
+{
+   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    
+    for(int count = 0;count <= word.length();word.substring(count)){
+        System.out.println("x");
+    }
+  }
+}
+       
+   
+   
